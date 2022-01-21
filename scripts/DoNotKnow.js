c782d74ac61c945a8b3a31bda1d965d99d2cf9a5
Daniel Bautista <dbautista9015@gmail.com>
2022-01-21 11:47:44 -0800

    Created elements

--- scripts/DoNotKnow.js
+++ scripts/DoNotKnow.js
@@ -1,4 +1,25 @@
 export default function DataToWebsite(cityInfoArr) {
-    
+
+    //Gets html elements
+    let day = document.getElementsByClassName('day');
+    let weatherIcon = document.getElementsByClassName('weatherIcon');
+    let nowTemp = document.getElementsByClassName('nowTemp');
+    let humid = document.getElementsByClassName('humid');
+    let wind = document.getElementsByClassName('wind');
+    let minMaxTemp = document.getElementsByClassName('minMaxTemp');
+    let date = document.getElementsByClassName('date');
+
+    for (let i = 0; i < cityInfoArr.length; i++) {
+        day[i].textContent = cityInfoArr[i].day;
+        //Will change weather Icon later
+
+        nowTemp[i].textContent = cityInfoArr[i].currentTemp + '°C';
+        humid[i].textContent = 'Humidity: ' + cityInfoArr[i].humidity + '%';
+        wind[i].textContent = 'Wind: ' + cityInfoArr[i].windSpeed + 'km/h';
+        minMaxTemp[i].textContent = cityInfoArr[i].maxTemp + '°C' + ' - ' + cityInfoArr[i].minTemp + '°C'
+        date[i].textContent = cityInfoArr[i].date;
+    }
+
+
 }
 
